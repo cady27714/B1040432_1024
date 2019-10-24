aaab320cdce29ded17d28380f48fdf7cd01118cf
cady27714 <54615342+cady27714@users.noreply.github.com>
2019-10-24 12:10:01 +0800

    B1040432_郭又慈

---
+++ MotionCapture/Assets/Robot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    private Animator robot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        robot = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            robot.SetTrigger("跳舞觸發器");
+            robot.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+        }
+    }
+}
